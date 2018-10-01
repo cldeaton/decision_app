d5e743a8f8f141627805fb1a2565318cd1869c44
Chris Deaton <chrisdeaton@Chriss-MacBook-Pro.local>
2018-10-01 10:05:14 -0400

    pre-mapBox integrations

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-var exec = require("child_process").exec;
-var inside = true;
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require('body-parser'),
+    exec       = require("child_process").exec;
 
-console.log(process.env.LATITUDE);
-console.log(process.env.LONGITUDE);
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+}); //Disables browser security to allow communication with localhost
 
-if (inside === false) {
-  console.log("Exiting!!");
-  process.exit();
-} else {
-  console.log("Here we go!!");
-  exec("USER_NUMBER=+17046898590 MESSAGE_BODY='HEY DO SOMETHING' node ../sms_app/index.js", function(err, stdout, stderror) {
+app.post("/", function(req, res){
+  console.log(req.body)
+  command = `USER_NUMBER='${req.body.userNumber}' MESSAGE_BODY='${req.body.messageBody}' node ../sms_app/index.js`;
+  console.log(command)
+  exec(command, function(err, stdout, stderror){
     console.log(stdout);
+    res.status(200).send("Message Sent");
   });
-}
+
+  console.log(req.body)
+})
+
+
+//listening Route
+app.listen(process.env.PORT || 2000, function(){
+    console.log("Server has started!!");
+});
